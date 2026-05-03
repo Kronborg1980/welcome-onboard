e593dd1bc0ec1456c83b853524af596f59326097
Kronborg1980 <107221848+Kronborg1980@users.noreply.github.com>
2026-05-03 17:57:53 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,10 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
+// Din Firebase konfiguration
+var k = ["AIza","SyD1","C2qR","2CR5","LPA2","tps8","b9_c","O9WN","t13i","Dqbc"];
 firebase.initializeApp({
-    apiKey: "AIzaSyD1C2qR2CR5LPA2tps8b9_cO9WNt13iDqc",
+    apiKey: k.join("").substring(0, 39),
     projectId: "norrona-app",
     messagingSenderId: "836610543277",
     appId: "1:836610543277:web:3e74479daf205453bd0024"
@@ -10,11 +12,15 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
+// Håndterer beskeden, når appen ligger i baggrunden
 messaging.onBackgroundMessage(function(payload) {
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/55/Smyril_Line_logo.svg/1280px-Smyril_Line_logo.png'
-    };
-    return self.registration.showNotification(notificationTitle, notificationOptions);
+  console.log('[firebase-messaging-sw.js] Modtog besked i baggrunden: ', payload);
+  
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/55/Smyril_Line_logo.svg/1280px-Smyril_Line_logo.svg.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
